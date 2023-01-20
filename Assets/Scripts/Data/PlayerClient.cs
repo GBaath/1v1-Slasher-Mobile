596f02c9dff642bb5056645739a59b71a71713b9
GVBååth <gustav.baath02@gmail.com>
2023-01-20 10:15:36 +0100

    slash is visible

--- Assets/Scripts/Data/PlayerClient.cs
+++ Assets/Scripts/Data/PlayerClient.cs
@@ -6,20 +6,17 @@ using Unity.Netcode;
 public class PlayerClient : NetworkBehaviour
 {
     public NetworkVariable<Vector2> relativeShieldPosition = new NetworkVariable<Vector2>(default,NetworkVariableReadPermission.Everyone,NetworkVariableWritePermission.Owner);
-    public Vector2 localRelativeShieldPos;
     private void Start()
     {
         OnPlayerJoin();
-        relativeShieldPosition.OnValueChanged += (oldVal, newVal) => 
-        { 
-            if(newVal != Vector2.zero)
-                localRelativeShieldPos = newVal; 
-        };
+
     }
 
     public void OnPlayerJoin()
     {
         GameManager.instance.OnPlayerJoin(GetComponent<PlayerData>());
+
+        //stupid canvas rendering overrides
         transform.parent = GameManager.instance.serverCanvas.transform;
         transform.position = Vector3.zero;
         transform.position += Vector3.forward*Camera.main.nearClipPlane;
--- Assets/Scripts/Inputs/SlashFollow.cs
+++ Assets/Scripts/Inputs/SlashFollow.cs
@@ -6,7 +6,7 @@ public class SlashFollow : MonoBehaviour
 {
     [SerializeField] LineRenderer line;
     [SerializeField] RectTransform slashInputRect;
-    Vector2 startPoint, endPoint;
+    Vector3 startPoint, endPoint;
 
 
     public Collider2D opponentShieldRef;
@@ -17,6 +17,7 @@ public class SlashFollow : MonoBehaviour
         if (Input.GetMouseButtonDown(0))
         {
             startPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            startPoint = new Vector3(startPoint.x, startPoint.y, Camera.main.nearClipPlane);
 
             //if (slashInputRect.rect.Contains(Input.mousePosition)) //touch within acceptable area
             {
@@ -27,6 +28,7 @@ public class SlashFollow : MonoBehaviour
         else if(Input.GetMouseButtonUp(0))
         {
             endPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            endPoint = new Vector3(endPoint.x, endPoint.y, Camera.main.nearClipPlane);
             ToggleFollow(false);
             CheckHit();
         }
@@ -34,6 +36,7 @@ public class SlashFollow : MonoBehaviour
         {
             //transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             endPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            endPoint = new Vector3(endPoint.x, endPoint.y, Camera.main.nearClipPlane);
             line.SetPosition(1, endPoint);
         }
     }
